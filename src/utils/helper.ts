a7579cac53928b026c2f70f84ade578aac5766d4
Jinyoung Park <cruiff79@gmail.com>
2025-09-30 15:10:37 -0700

    update date format

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+
 export const apiUrl = import.meta.env.VITE_API_URL;
 
 export const menuItems = [
@@ -6,3 +8,7 @@ export const menuItems = [
   {label: 'STATS', link: '/stats'},
   {label: 'TEAMS', link: '/teams'},
 ];
+
+export const formatDate = (date: Date) => {
+  return format(new Date(date), "yy-MM-dd (EEE) hh:mm:ss a");
+}
